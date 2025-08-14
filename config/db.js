f43904003bf69490c63e84c3f30a3b94e33c2bc7
Gyanibabastore <gyanibabastore.com@gmail.com>
2025-08-14 22:50:38 +0530

    Update db.js
    db fix

--- config/db.js
+++ config/db.js
@@ -6,10 +6,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI); // ✅ Cleaned version
 
     console.log('✅ MongoDB Connected (Atlas)');
   } catch (err) {
